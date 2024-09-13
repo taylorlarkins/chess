72cacddade064803c19ca021a568500873f8f65a
Taylor Larkins <tlarki7949@gmail.com>
2024-09-13 13:01:11 -0600

    Promotion configuration

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -104,6 +104,7 @@ public class ChessPiece {
                 } else {
                     break;
                 }
+                //add promotion logic
             }
         }
         return moves;
