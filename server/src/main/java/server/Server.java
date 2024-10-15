96e2a60356a426f6754078a4708469aea66aa4e1
Taylor Larkins <tlarki7949@gmail.com>
2024-10-15 16:31:40 -0600

    Working hardcoded endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -10,7 +10,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.get("/game", this::listGames);
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
@@ -21,5 +21,9 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object listGames(Request req, Response res) {
+        return "{ \"games\": [{\"gameID\": 1234, \"whiteUsername\":\"\", \"blackUsername\":\"\", \"gameName:\"\"} ]}";
     }
 }
