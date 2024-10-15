eab80181d8a13c601b22d2ddc9b694e35a65f136
Taylor Larkins <tlarki7949@gmail.com>
2024-10-15 16:50:07 -0600

    Registered endpoints with hardcoded returns

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,6 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
 
 public class Server {
@@ -10,7 +11,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.delete("/db", this::deleteData);
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
         Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
@@ -23,7 +30,31 @@ public class Server {
         Spark.awaitStop();
     }
 
-    private Object listGames(Request req, Response res) {
-        return "{ \"games\": [{\"gameID\": 1234, \"whiteUsername\":\"\", \"blackUsername\":\"\", \"gameName:\"\"} ]}";
+    private String deleteData(Request req, Response res) {
+        return new Gson().toJson("DELETE /db not implemented");
+    }
+
+    private String registerUser(Request req, Response res) {
+        return new Gson().toJson("POST /user not implemented");
+    }
+
+    private String login(Request req, Response res) {
+        return new Gson().toJson("POST /session not implemented");
+    }
+
+    private String logout(Request req, Response res) {
+        return new Gson().toJson("DELETE /session not implemented");
+    }
+
+    private String listGames(Request req, Response res) {
+        return new Gson().toJson("GET /game not implemented");
+    }
+
+    private String createGame(Request req, Response res) {
+        return new Gson().toJson("POST /game not implemented");
+    }
+
+    private String joinGame(Request req, Response res) {
+        return new Gson().toJson("PUT /game not implemented");
     }
 }
